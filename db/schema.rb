faaeac1e81269e33b5319c76b5a20609cbe3058c
Erin Page <erin@reax.io>
2016-06-03 15:19:06 -0500

    submission model for applying to a job

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160520213231) do
+ActiveRecord::Schema.define(version: 20160603200802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 20160520213231) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "submissions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "job_opening_id"
+    t.integer  "aasm_state"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "submissions", ["job_opening_id"], name: "index_submissions_on_job_opening_id", using: :btree
+  add_index "submissions", ["user_id"], name: "index_submissions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -44,4 +55,6 @@ ActiveRecord::Schema.define(version: 20160520213231) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "submissions", "job_openings"
+  add_foreign_key "submissions", "users"
 end
