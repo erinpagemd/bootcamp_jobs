61fd99b4fce67fe0c6fb61f9407e29336671aa1a
Erin Page <erin@reax.io>
2016-07-08 18:13:53 -0500

    define job_name in decorator

--- app/decorators/submission_decorator.rb
+++ app/decorators/submission_decorator.rb
@@ -5,4 +5,8 @@ class SubmissionDecorator < Draper::Decorator
     return "Information not available" unless object.created_at
     "Applied on " + object.created_at.strftime("%A, %B %e, %Y")
   end
+
+  def job_name
+    object.try(:job_opening).try(:name) || "Some Job"
+  end
 end
