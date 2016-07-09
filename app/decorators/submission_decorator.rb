4c1e5043b59d404777292bfab42d91bc1eaa418c
Erin Page <erin@reax.io>
2016-07-09 18:21:39 -0500

    use decorator to show applicant_name

--- app/decorators/submission_decorator.rb
+++ app/decorators/submission_decorator.rb
@@ -1,6 +1,11 @@
 class SubmissionDecorator < Draper::Decorator
   delegate_all
 
+  def applicant_name
+    return "Some Applicant" unless object.user
+    "#{object.user.first_name} #{object.user.last_name}"
+  end
+
   def applied_date_message
     return "Information not available" unless object.created_at
     "Applied on " + object.created_at.strftime("%A, %B %e, %Y")
