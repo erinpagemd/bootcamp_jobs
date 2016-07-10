ca3a2cb36f066759fb90ef0e9fb8389e9c898c33
Erin <erinpagemd@yahoo.com>
2016-07-10 10:44:33 -0500

    Admin contacts applicant (#10)
    * better conditional if name elements not present
    
    * show message if no phone numbers
    
    * use try method
    
    * state machine include admin contact, interview, hire
    
    * better name for link
    
    * move email, conditionals to show action buttons
    
    * active will include more than state of applied
    
    * method, route for admin contacts candidate
    
    * can reject if in other states
    
    * more consisted disable with message

--- app/decorators/submission_decorator.rb
+++ app/decorators/submission_decorator.rb
@@ -1,9 +1,13 @@
 class SubmissionDecorator < Draper::Decorator
   delegate_all
 
+  def applicant_email
+    "#{object.user.email}"
+  end
+
   def applicant_name
-    return "Some Applicant" unless object.user
-    "#{object.user.first_name} #{object.user.last_name}"
+    return "Some Anonymous Applicant" unless object.user && object.user.first_name || object.user.last_name
+    "#{object.user.try(:first_name)} #{object.user.try(:last_name)}"
   end
 
   def applied_date_message
@@ -13,5 +17,9 @@ class SubmissionDecorator < Draper::Decorator
 
   def job_name
     object.try(:job_opening).try(:name) || "Some Job"
+  end
+
+  def without_phone_numbers
+    "Applicant did not enter phone number" if object.user.phone_numbers.empty?
   end
 end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,20 +2,39 @@ class Submission < ActiveRecord::Base
   belongs_to :user
   belongs_to :job_opening
 
-  scope :active, -> { where(aasm_state: 0) }
+  scope :active, -> { where(aasm_state: [0, 2, 3]) }
 
   include AASM
 
   enum aasm_state: {
     applied: 0,
-    rejected: 1
+    rejected: 1,
+    contacted: 2,
+    interviewed: 3,
+    hired: 4
   }
 
   aasm column: :aasm_state, enum: true do
     Submission.aasm_states.each { |k, v| state k.to_sym, inital: v.zero? }
 
     event :admin_reject do
-      transitions :from => :applied, :to => :rejected
+      transitions :from => [:applied, :contacted, :interviewed], :to => :rejected
+    end
+
+    event :admin_contacts_candidate do
+      transitions :from => :applied, :to => :contacted
+    end
+
+    event :admin_interviews_candidate do
+      transitions :from => [:applied, :contacted], :to => :interviewed
+    end
+
+    event :admin_hires_candidate do
+      transitions :from => :interviewed, :to => :hired
+    end
+
+    event :admin_reconsiders_candidate do
+      transitions :from => :rejected, :to => :applied
     end
   end
 end
