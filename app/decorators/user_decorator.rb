ef94a9f224f338512b457dbcae0edc6f3bb460a6
Erin Page <erin@reax.io>
2016-07-10 01:46:46 -0500

    move message logic to decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -5,4 +5,8 @@ class UserDecorator < Draper::Decorator
     return "Profile Incomplete" unless object.first_name || object.last_name
     "#{object.first_name} #{object.last_name}"
   end
+
+  def without_phone_numbers
+    h.content_tag(:h4, "Please enter contact information") if object.phone_numbers.empty?
+  end
 end
