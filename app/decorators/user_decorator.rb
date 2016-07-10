ba6a4b62499ec487fb69a967683feb64b76512bf
Erin <erinpagemd@yahoo.com>
2016-07-10 01:52:50 -0500

    Improvement/applicant placeholders (#9)
    * show that profile is incomplete if first or last name are not present
    
    * conditionally show phone numbers
    
    * show message if no phone_numbers
    
    * show different message if submissions
    
    * better name for button link
    
    * move message logic to decorator
    
    * move header logic to decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,6 +2,19 @@ class UserDecorator < Draper::Decorator
   delegate_all
 
   def name
+    return "Profile Incomplete" unless object.first_name || object.last_name
     "#{object.first_name} #{object.last_name}"
+  end
+
+  def without_phone_numbers
+    h.content_tag(:h4, "Please enter contact information") if object.phone_numbers.empty?
+  end
+
+  def submissions_header
+    if object.submissions.any?
+      h.content_tag(:h2, "My Applications")
+    else
+      h.content_tag(:h3, "You have not applied for any jobs yet! Get started.")
+    end
   end
 end
