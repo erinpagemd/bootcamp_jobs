e6a1999dcd245528d992037efb37019ea0dccda6
Erin Page <erin@reax.io>
2016-07-09 19:48:26 -0500

    show total applications and active applications

--- app/decorators/job_opening_decorator.rb
+++ app/decorators/job_opening_decorator.rb
@@ -1,9 +1,13 @@
 class JobOpeningDecorator < Draper::Decorator
   delegate_all
 
-  def applicant_count
-    return "No applicants" unless object.submissions.any?
-    "#{object.submissions.try(:count)} applicants"
+  def active_applications
+    return "" unless object.submissions.any?
+    "#{object.submissions.active.count} active applications"
   end
 
+  def application_count
+    return "No applications" unless object.submissions.any?
+    "#{object.submissions.try(:count)} applications"
+  end
 end
