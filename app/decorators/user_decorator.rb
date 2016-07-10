37bb49df5a3c80e04576d9dd0086846dec7e9b87
Erin Page <erin@reax.io>
2016-07-10 01:51:33 -0500

    move header logic to decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -9,4 +9,12 @@ class UserDecorator < Draper::Decorator
   def without_phone_numbers
     h.content_tag(:h4, "Please enter contact information") if object.phone_numbers.empty?
   end
+
+  def submissions_header
+    if object.submissions.any?
+      h.content_tag(:h2, "My Applications")
+    else
+      h.content_tag(:h3, "You have not applied for any jobs yet! Get started.")
+    end
+  end
 end
