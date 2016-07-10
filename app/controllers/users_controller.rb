2ae8642cb3020fc55f2f775e831739cb8724b029
Erin Page <erin@reax.io>
2016-07-09 20:44:01 -0500

    update action in user controller, define user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,15 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  def update
+    @user.update(user_params)
+    redirect_to job_openings_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
+  end
 end
