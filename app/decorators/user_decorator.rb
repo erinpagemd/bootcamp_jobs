bac6d327820f3f8fa5af9dc9d25fa068865ebcd6
Erin Page <erin@reax.io>
2016-07-10 00:55:31 -0500

    show that profile is incomplete if first or last name are not present

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,6 +2,7 @@ class UserDecorator < Draper::Decorator
   delegate_all
 
   def name
+    return "Profile Incomplete" unless object.first_name || object.last_name
     "#{object.first_name} #{object.last_name}"
   end
 end
