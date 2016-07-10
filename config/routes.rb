178429dda15452ac96b79a708a85de8f912a847e
Erin Page <erin@reax.io>
2016-07-09 19:32:42 -0500

    add admin/submissions_controller with method and route to handle event admin_reject

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :job_openings
+    resources :submissions do
+      member do
+        put :admin_reject
+      end
+    end
   end
 
   resources :job_openings
